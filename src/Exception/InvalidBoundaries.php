a244f5a08520768ec98d6a802d73037a6043ce2a
Leo Viezens <leo.viezens@web.de>
2022-05-21 19:50:53 +0200

    Upgrade dependencies, fix deprecations (#4)

--- src/Exception/InvalidBoundaries.php
+++ src/Exception/InvalidBoundaries.php
@@ -5,17 +5,14 @@ declare(strict_types=1);
 namespace LeoVie\PhpFilesystem\Exception;
 
 use Exception;
-use Safe\Exceptions\StringsException;
 
 class InvalidBoundaries extends Exception
 {
-    /** @throws StringsException */
     private function __construct(int $start, int $end)
     {
-        parent::__construct(\Safe\sprintf('Start boundary %s is greater than end boundary %s.', $start, $end));
+        parent::__construct(sprintf('Start boundary %s is greater than end boundary %s.', $start, $end));
     }
 
-    /** @throws StringsException */
     public static function create(int $start, int $end): self
     {
         return new self($start, $end);
