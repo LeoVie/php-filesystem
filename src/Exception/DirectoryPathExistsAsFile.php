2d0eefbfc12fc938cd23c207ccd4aacc15217533
Leo Viezens <leo.viezens@web.de>
2022-05-21 19:48:48 +0200

    Upgrade dependencies, fix deprecations

--- src/Exception/DirectoryPathExistsAsFile.php
+++ src/Exception/DirectoryPathExistsAsFile.php
@@ -9,13 +9,11 @@ use Safe\Exceptions\StringsException;
 
 class DirectoryPathExistsAsFile extends Exception
 {
-    /** @throws StringsException */
     private function __construct(string $path)
     {
-        parent::__construct(\Safe\sprintf('Directory path "%s" exists as file.', $path));
+        parent::__construct(sprintf('Directory path "%s" exists as file.', $path));
     }
 
-    /** @throws StringsException */
     public static function create(string $path): self
     {
         return new self($path);
